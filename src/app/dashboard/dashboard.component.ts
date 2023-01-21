88bd3357f3da304310d6e176872da71b9232ee77
trainandhire <projects.trainandhire@gmail.com>
2023-01-21 11:08:34 +0530

    login complted

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
 
-  constructor() { }
+  constructor(private _router:Router) { }
 
-  ngOnInit(): void {
+  logout(){
+    this._router.navigateByUrl("/login");
   }
+
 
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Route, Router } from '@angular/router';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  public loginForm: FormGroup = new FormGroup({
+    email: new FormControl(),
+    password: new FormControl()
+  })
 
-  ngOnInit(): void {
+  constructor(private _loginService: LoginService, private _router:Router){}
+
+  login(){
+    console.log(this.loginForm.value);
+    this._loginService.login(this.loginForm.value).subscribe(
+      (data:any)=>{
+        alert("Login Succesfful");
+        // navigte to dash
+        this._router.navigateByUrl("/dashboard");
+
+      },
+      (err:any)=>{
+        alert("invalid credentials");
+      }
+    )
   }
+
+
 
 }
